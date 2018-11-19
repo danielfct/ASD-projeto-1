90a1038f70eb84a39b937eea91d8bfaa75229464
Daniel Pimenta <d.pimenta@campus.fct.unl.pt>
2018-11-19 07:02:27 +0000

    Older versions deleted

--- Source code/src/Sequential/Main.scala
+++
@@ -1,9 +0,0 @@
-import akka.actor.ActorSystem;
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    // Create the actor system that will spawn our actors
-    val actorSystem = ActorSystem("Publish_Subscribe_on_Chord");
-    
-  }
-}
