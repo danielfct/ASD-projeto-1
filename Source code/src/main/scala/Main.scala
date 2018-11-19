6f950e36028af8cd445079bd49add7f5d644c361
Daniel Pimenta <d.pimenta@campus.fct.unl.pt>
2018-11-19 05:48:27 +0000

    Fixed main

--- Source code/src/main/scala/Main.scala
+++ Source code/src/main/scala/Main.scala
@@ -4,17 +4,13 @@ import akka.actor.{ActorSystem, Props}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    /*if (args.length != 3) {
-      println("Usage: sbt run NumberOfNodes NumberOfRequests NodeFailurePercentage")
+    if (args.length != 2) {
+      println("Usage: sbt run NumberOfNodes NumberOfRequests")
       System.exit(1)
     }
     val maxNrNodes: Int = args(0).toInt
     val numberOfRequests: Int = math.max(10, args(1).toInt)
-    val nodeFailurePercentage: Float = math.min(args(1).toFloat, 0.9f)
-    */
-    val maxNrNodes: Int = 100
-    val numberOfRequests: Int = 10
-    val nodeFailurePercentage: Float = 0f //TODO para suportar falhas é preciso modificar o chord (successorList)
+    val nodeFailurePercentage: Float = 0.0f
 
     val actorSystem = ActorSystem("PublishSubscribeChord")
     val tester = actorSystem.actorOf(Props(new ChordTester(maxNrNodes, numberOfRequests, nodeFailurePercentage)), "PublishSubscribeChordTester")
